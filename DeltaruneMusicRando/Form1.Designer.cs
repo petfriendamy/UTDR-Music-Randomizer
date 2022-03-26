6ae3cbb840bc49000aa76705d7bba6e4d24d25dd
petfriendamy <petfriendamy@gmail.com>
2022-03-26 14:01:29 -0600

    Initial commit

---
+++ DeltaruneMusicRando/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace DeltaruneMusicRando
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMusFolder = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBoxOptions = new System.Windows.Forms.GroupBox();
+            this.checkBoxSFX = new System.Windows.Forms.CheckBox();
+            this.checkBoxMultiPart = new System.Windows.Forms.CheckBox();
+            this.checkBoxCyberBattle = new System.Windows.Forms.CheckBox();
+            this.checkBoxCredits = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmbience = new System.Windows.Forms.CheckBox();
+            this.checkBoxSpeedrunLegal = new System.Windows.Forms.CheckBox();
+            this.buttonRandomize = new System.Windows.Forms.Button();
+            this.groupBoxOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxMusFolder
+            // 
+            this.textBoxMusFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMusFolder.Location = new System.Drawing.Point(12, 27);
+            this.textBoxMusFolder.Name = "textBoxMusFolder";
+            this.textBoxMusFolder.Size = new System.Drawing.Size(329, 23);
+            this.textBoxMusFolder.TabIndex = 0;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowse.Location = new System.Drawing.Point(347, 27);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Browse...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please locate Undertale/Deltarune\'s install folder:";
+            // 
+            // groupBoxOptions
+            // 
+            this.groupBoxOptions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxOptions.Controls.Add(this.checkBoxSFX);
+            this.groupBoxOptions.Controls.Add(this.checkBoxMultiPart);
+            this.groupBoxOptions.Controls.Add(this.checkBoxCyberBattle);
+            this.groupBoxOptions.Controls.Add(this.checkBoxCredits);
+            this.groupBoxOptions.Controls.Add(this.checkBoxAmbience);
+            this.groupBoxOptions.Controls.Add(this.checkBoxSpeedrunLegal);
+            this.groupBoxOptions.Location = new System.Drawing.Point(12, 56);
+            this.groupBoxOptions.Name = "groupBoxOptions";
+            this.groupBoxOptions.Size = new System.Drawing.Size(410, 184);
+            this.groupBoxOptions.TabIndex = 3;
+            this.groupBoxOptions.TabStop = false;
+            this.groupBoxOptions.Text = "Options";
+            // 
+            // checkBoxSFX
+            // 
+            this.checkBoxSFX.AutoSize = true;
+            this.checkBoxSFX.Location = new System.Drawing.Point(6, 147);
+            this.checkBoxSFX.Name = "checkBoxSFX";
+            this.checkBoxSFX.Size = new System.Drawing.Size(354, 19);
+            this.checkBoxSFX.TabIndex = 5;
+            this.checkBoxSFX.Text = "Include jingles and sound effects (may have VERY BAD results)";
+            this.checkBoxSFX.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMultiPart
+            // 
+            this.checkBoxMultiPart.AutoSize = true;
+            this.checkBoxMultiPart.Location = new System.Drawing.Point(6, 72);
+            this.checkBoxMultiPart.Name = "checkBoxMultiPart";
+            this.checkBoxMultiPart.Size = new System.Drawing.Size(273, 19);
+            this.checkBoxMultiPart.TabIndex = 2;
+            this.checkBoxMultiPart.Text = "Include Your Best Nightmare + Finale (UT only)";
+            this.checkBoxMultiPart.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCyberBattle
+            // 
+            this.checkBoxCyberBattle.AutoSize = true;
+            this.checkBoxCyberBattle.Location = new System.Drawing.Point(6, 47);
+            this.checkBoxCyberBattle.Name = "checkBoxCyberBattle";
+            this.checkBoxCyberBattle.Size = new System.Drawing.Size(327, 19);
+            this.checkBoxCyberBattle.TabIndex = 1;
+            this.checkBoxCyberBattle.Text = "Include cyber_battle_prelude.ogg (DR only, may softlock)";
+            this.checkBoxCyberBattle.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCredits
+            // 
+            this.checkBoxCredits.AutoSize = true;
+            this.checkBoxCredits.Location = new System.Drawing.Point(6, 97);
+            this.checkBoxCredits.Name = "checkBoxCredits";
+            this.checkBoxCredits.Size = new System.Drawing.Size(137, 19);
+            this.checkBoxCredits.TabIndex = 3;
+            this.checkBoxCredits.Text = "Include credits songs";
+            this.checkBoxCredits.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmbience
+            // 
+            this.checkBoxAmbience.AutoSize = true;
+            this.checkBoxAmbience.Location = new System.Drawing.Point(6, 122);
+            this.checkBoxAmbience.Name = "checkBoxAmbience";
+            this.checkBoxAmbience.Size = new System.Drawing.Size(120, 19);
+            this.checkBoxAmbience.TabIndex = 4;
+            this.checkBoxAmbience.Text = "Include ambience";
+            this.checkBoxAmbience.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSpeedrunLegal
+            // 
+            this.checkBoxSpeedrunLegal.AutoSize = true;
+            this.checkBoxSpeedrunLegal.Location = new System.Drawing.Point(6, 22);
+            this.checkBoxSpeedrunLegal.Name = "checkBoxSpeedrunLegal";
+            this.checkBoxSpeedrunLegal.Size = new System.Drawing.Size(323, 19);
+            this.checkBoxSpeedrunLegal.TabIndex = 0;
+            this.checkBoxSpeedrunLegal.Text = "Include cyber.ogg and mansion.ogg (only applies to DR)";
+            this.checkBoxSpeedrunLegal.UseVisualStyleBackColor = true;
+            // 
+            // buttonRandomize
+            // 
+            this.buttonRandomize.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRandomize.Location = new System.Drawing.Point(12, 246);
+            this.buttonRandomize.Name = "buttonRandomize";
+            this.buttonRandomize.Size = new System.Drawing.Size(410, 23);
+            this.buttonRandomize.TabIndex = 4;
+            this.buttonRandomize.Text = "Randomize!";
+            this.buttonRandomize.UseVisualStyleBackColor = true;
+            this.buttonRandomize.Click += new System.EventHandler(this.buttonRandomize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 281);
+            this.Controls.Add(this.buttonRandomize);
+            this.Controls.Add(this.groupBoxOptions);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxMusFolder);
+            this.MinimumSize = new System.Drawing.Size(450, 320);
+            this.Name = "Form1";
+            this.Text = "Undertale/Deltarune Music Randomizer";
+            this.groupBoxOptions.ResumeLayout(false);
+            this.groupBoxOptions.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBoxMusFolder;
+        private Button buttonBrowse;
+        private Label label1;
+        private GroupBox groupBoxOptions;
+        private CheckBox checkBoxAmbience;
+        private CheckBox checkBoxSpeedrunLegal;
+        private Button buttonRandomize;
+        private CheckBox checkBoxCredits;
+        private CheckBox checkBoxCyberBattle;
+        private CheckBox checkBoxMultiPart;
+        private CheckBox checkBoxSFX;
+    }
+}
---
+++ DeltaruneMusicRando/Form1.cs
@@ -0,0 +1,327 @@
+namespace DeltaruneMusicRando
+{
+    public partial class Form1 : Form
+    {
+        private static string[] SpeedrunIllegalSongs = new string[]
+        {
+            "cyber", "mansion"
+        };
+
+        private static string[] CreditsSongsDeltarune = new string[]
+        {
+            "dontforget", "ch2_credits"
+        };
+
+        private static string[] CreditsSongsUndertale = new string[]
+        {
+            "mus_cast_1", "mus_cast_2", "mus_cast_3", "mus_cast_4",
+            "mus_cast_5", "mus_cast_6", "mus_cast_7"
+        };
+
+        private static string[] FloweyFightSongs = new string[]
+        {
+            "mus_f_intro", "mus_f_part1", "mus_f_part2", "mus_f_part3",
+            "mus_repeat_1", "mus_repeat_2", "mus_f_6s_1", "mus_f_6s_2",
+            "mus_f_6s_3", "mus_f_6s_4", "mus_f_6s_5", "mus_f_6s_6",
+            "mus_f_saved", "mus_f_finale_1", "mus_f_finale_1_l",
+            "mus_f_finale_2", "mus_f_finale_3",
+        };
+
+        private static string[] AmbienceDeltarune = new string[]
+        {
+            "alley_ambience", "audio_drone", "bird", "deep_noise", "elevator",
+            "honksong", "mus_birdnoise", "ocean", "shinkansen", "w",
+            "wind_highplace", "wind"
+        };
+
+        private static string[] AmbienceUndertale = new string[]
+        {
+            "mus_ambientwater", "mus_barrier", "mus_bgflamea", "mus_birdnoise",
+            "mus_core_ambience", "mus_crickets", "mus_deeploop2", "mus_drone",
+            "mus_elevator", "mus_elevator_last", "mus_f_wind1", "mus_f_wind2",
+            "mus_oogloop", "mus_rain", "mus_rain_deep", "mus_tone2", "mus_tone3",
+            "mus_wind", "mus_zzz_c", "mus_zzz_c2"
+        };
+
+        private static string[] SoundsDeltarune = new string[]
+        {
+            "berdly_audience", "berdly_descend", "charjoined", "fanfare",
+            "queen_intro", "s_neo_clip", "sink_noise", "spamton_laugh_noise",
+            "static_placeholder", "tv_noise"
+        };
+
+        private static string[] SoundsUndertale = new string[]
+        {
+            "abc_123_a", "mus_alphysfix", "mus_bergentruckung", "mus_churchbell",
+            "mus_computer", "mus_cymbal", "mus_dogmeander", "mus_doorclose",
+            "mus_dooropen", "mus_drone", "mus_dununnn", "mus_f_alarm", "mus_f_destroyed",
+            "mus_f_destroyed2", "mus_f_destroyed3", "mus_f_newlaugh", "mus_f_newlaugh_low",
+            "mus_fearsting", "mus_harpnoise", "mus_intronoise", "mus_mett_applause",
+            "mus_mett_cheer", "mus_mode", "mus_myemeow", "mus_ohyes", "mus_rimshot",
+            "mus_sfx_a_grab", "mus_sfx_chainsaw", "mus_sfx_hypergoner_charge",
+            "mus_sfx_hypergoner_laugh", "mus_sfx_hypergoner_hold", "mus_snowwalk",
+            "mus_sticksnap", "mus_wawa", "mus_whoopee", "snd_ballchime", "snd_bombfall",
+            "snd_bombsplosion", "snd_buzzing", "snd_curtgunshot", "snd_fall2", "snd_flameloop",
+            "snd_heavydamage", "snd_mushroomdance"
+        };
+
+        private struct SongFile
+        {
+            public SongFile(string name, bool fromBaseFolder = false)
+            {
+                Name = name;
+                FromBaseFolder = fromBaseFolder;
+            }
+            public string Name { get; }
+            public bool FromBaseFolder { get; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsValidPath(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                if (File.Exists(path + @"\UNDERTALE.exe"))
+                {
+                    return true;
+                }
+                else if (File.Exists(path + @"\DELTARUNE.exe") && Directory.Exists(path + @"\mus"))
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            string path;
+
+            using (var search = new FolderBrowserDialog())
+            {
+                result = search.ShowDialog();
+                path = search.SelectedPath;
+            }
+
+            if (result == DialogResult.OK)
+            {
+                if (Directory.Exists(path))
+                {
+                    if (IsValidPath(path))
+                    {
+                        textBoxMusFolder.Text = path;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select a valid install path.", "Invalid Path",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Directory doesn't exist.", "Error", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonRandomize_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBoxMusFolder.Text))
+            {
+                MessageBox.Show("Directory doesn't exist.", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else if (!IsValidPath(textBoxMusFolder.Text))
+            {
+                MessageBox.Show("Please select a valid install path.", "Invalid Path",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                string musicPath = textBoxMusFolder.Text;
+                bool isDeltarune = false;
+                if (File.Exists(textBoxMusFolder.Text + @"\DELTARUNE.exe"))
+                {
+                    musicPath += @"\mus";
+                    isDeltarune = true;
+                }
+                string[] files = Directory.GetFiles(musicPath);
+                IEnumerable<string>? oggFiles;
+                if (isDeltarune)
+                {
+                    oggFiles =
+                        from f in files
+                        where Path.GetExtension(f) == ".ogg"
+                        where (checkBoxSpeedrunLegal.Checked ? true : !SpeedrunIllegalSongs.Contains(Path.GetFileNameWithoutExtension(f).ToLower()))
+                        where (checkBoxCyberBattle.Checked ? true : Path.GetFileNameWithoutExtension(f) != "cyber_battle_prelude")
+                        where (checkBoxCredits.Checked ? true : !CreditsSongsDeltarune.Contains(Path.GetFileNameWithoutExtension(f).ToLower()))
+                        where (checkBoxAmbience.Checked ? true : !AmbienceDeltarune.Contains(Path.GetFileNameWithoutExtension(f).ToLower()))
+                        where (checkBoxSFX.Checked ? true : !SoundsDeltarune.Contains(Path.GetFileNameWithoutExtension(f).ToLower()))
+                        select f;
+                }
+                else //Undertale
+                {
+                    oggFiles =
+                        from f in files
+                        where Path.GetExtension(f) == ".ogg"
+                        where Path.GetFileNameWithoutExtension(f) != "mus_silence"
+                        where Path.GetFileNameWithoutExtension(f) != "mus_story_stuck"
+                        where (checkBoxMultiPart.Checked ? true : !FloweyFightSongs.Contains(Path.GetFileNameWithoutExtension(f).ToLower()))
+                        where (checkBoxCredits.Checked ? true : !CreditsSongsUndertale.Contains(Path.GetFileNameWithoutExtension(f).ToLower()))
+                        where (checkBoxAmbience.Checked ? true : !AmbienceUndertale.Contains(Path.GetFileNameWithoutExtension(f).ToLower()))
+                        where (checkBoxSFX.Checked ? true : !SoundsUndertale.Contains(Path.GetFileNameWithoutExtension(f).ToLower()))
+                        select f;
+                }
+
+                if (!oggFiles.Any())
+                {
+                    MessageBox.Show("No valid OGG files found.", "Error", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+                else
+                {
+                    try //attempt to make a backup folder
+                    {
+                        bool makeBackup = false;
+                        string temp = musicPath + @"\backup";
+                        if (Directory.Exists(temp))
+                        {
+                            var result = MessageBox.Show("Backup folder already exists. Overwrite it? (If you have randomized previously, this will backup the randomized files!)",
+                                "Overwrite Backup?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                            if (result == DialogResult.Cancel)
+                            {
+                                return;
+                            }
+                            else if (result == DialogResult.Yes)
+                            {
+                                makeBackup = true;
+                            }
+                        }
+                        else
+                        {
+                            makeBackup = true;
+                        }
+                        var backupFolder = Directory.CreateDirectory(temp);
+
+                        try //attempt to move files to the backup folder
+                        {
+                            var tempNames = new List<SongFile>();
+
+                            foreach (var f in oggFiles)
+                            {
+                                temp = Path.GetFileName(f);
+                                tempNames.Add(new SongFile(temp));
+                                if (makeBackup || !File.Exists(backupFolder.FullName + @"\" + temp))
+                                {
+                                    File.Move(f, backupFolder.FullName + @"\" + temp, true);
+                                }
+                                else
+                                {
+                                    File.Delete(f);
+                                }
+                            }
+
+                            if (isDeltarune && checkBoxSFX.Checked) //grab SFX from base folder
+                            {
+                                var drFiles = Directory.GetFiles(textBoxMusFolder.Text);
+                                var drSounds =
+                                    from f in drFiles
+                                    where Path.GetExtension(f) == ".ogg"
+                                    select f;
+
+                                foreach (var f in drSounds)
+                                {
+                                    temp = Path.GetFileName(f);
+                                    tempNames.Add(new SongFile(temp, true));
+                                    if (makeBackup || !File.Exists(backupFolder.FullName + @"\" + temp))
+                                    {
+                                        File.Move(f, backupFolder.FullName + @"\" + temp, true);
+                                    }
+                                    else
+                                    {
+                                        File.Delete(f);
+                                    }
+                                }
+                            }
+
+                            var rng = new Random();
+                            int i = 0;
+                            foreach (var f in backupFolder.GetFiles())
+                            {
+                                if (oggFiles.Contains((musicPath + @"\" + f.Name).Replace(@"\\", @"\")) ||
+                                    (isDeltarune && checkBoxSFX.Checked &&
+                                    (f.Name.StartsWith("snd_") || f.Name.StartsWith("audio_intronoise"))))
+                                {
+                                    do
+                                    {
+                                        i = rng.Next(tempNames.Count);
+                                    } while (tempNames[i].Name == Path.GetFileName(f.Name));
+
+                                    if (tempNames[i].FromBaseFolder)
+                                    {
+                                        temp = textBoxMusFolder.Text + @"\" + tempNames[i].Name;
+                                    }
+                                    else
+                                    {
+                                        temp = musicPath + @"\" + tempNames[i].Name;
+                                    }
+                                    File.Copy(f.FullName, temp);
+                                    tempNames.RemoveAt(i);
+                                }
+                            }
+
+                            temp = "Randomization complete!";
+                            if (makeBackup)
+                            {
+                                temp += " Original files were copied to ";
+                                if (isDeltarune) { temp += @"\mus"; }
+                                temp += @"\backup";
+                            }
+                            MessageBox.Show(temp, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch //something went wrong while moving files
+                        {
+                            try //attempt to restore from the backup folder
+                            {
+                                foreach (var f in backupFolder.GetFiles())
+                                {
+                                    string destPath = musicPath;
+                                    if (isDeltarune && (f.Name.StartsWith("snd_") || f.Name.StartsWith("audio_intronoise")))
+                                    {
+                                        destPath = textBoxMusFolder.Text;
+                                    }
+                                    destPath += @"\" + f.Name;
+
+                                    if (File.Exists(destPath))
+                                    {
+                                        File.Delete(destPath);
+                                    }
+                                    File.Copy(f.FullName, destPath);
+                                }
+                                MessageBox.Show($"An error ocurred while randomizing. Files were recovered from backup.",
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            catch //something went wrong while restoring the backup
+                            {
+                                MessageBox.Show("A fatal error occurred, and data could not be recovered. Please reinstall the game.",
+                                    "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                    catch //failed to make backup folder
+                    {
+                        MessageBox.Show($"Failed to make backup folder. Please check your write permissions.",
+                            "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
